005729a4375c664c13f539fc869d6e87e6f83af7
George Villasboas <george@logics.com.br>
2014-04-25 16:16:44 -0300

    Removendo parametros do carrier ware

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+    :aws_access_key_id      => '',
+    :aws_secret_access_key  => '',
     :endpoint 				=> 'http://s3.amazonaws.com'
   }
   config.fog_directory  = ENV['AWS_FOG_DIRECTORY']
