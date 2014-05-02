ac840adae3c64386d36c83db2e9f4aafae7911d6
George Villasboas <george@logics.com.br>
2014-05-02 12:46:17 -0300

    Programacao de paginas internas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,8 @@ module ApplicationHelper
 		class_name = current_page?(link_path) ? "active" : ""
 
 		content_tag(:li, :class => class_name) do
-			link_to link_text, link_path
+			concat link_to link_text, link_path
+			concat content_tag(:div, "&nbsp;".html_safe, :class => 'separator')
 		end
 	end
 
