63893ccb77b1d1dc2a5c888b42a6515a9653620b
George Villasboas <george@logics.com.br>
2014-05-13 23:57:34 -0300

    Correcao do bug do gravatar no menu do backend

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -11,6 +11,12 @@ class AdminUser < ActiveRecord::Base
 	# 4. Authenticate the user :)
 	has_secure_password
 
+  	has_gravatar :email,
+  				 :secure => true,
+  				 :default => "https://s3.amazonaws.com/content.cocoaheads.com.br/images/anonymous.png",
+  				 :size => 120,
+  				 :rating => 'R'
+
 	# has_and_belongs_to_many :pages
 	# has_many :section_edits
 	# has_many :sections, :through => :section_edits
