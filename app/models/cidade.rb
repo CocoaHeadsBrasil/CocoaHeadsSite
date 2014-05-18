beebb3379f023ed726de61e47574d384cbfb0f0c
George Villasboas <george@logics.com.br>
2014-05-18 08:37:07 -0300

    Correcao de um bug nas cidades para esta nova versao do rails 4.1.1

--- app/models/cidade.rb
+++ app/models/cidade.rb
@@ -26,7 +26,7 @@ class Cidade < ActiveRecord::Base
 	            :format => EMAIL_REGEX
 
 	scope :ordenados, lambda { order("cidades.cidade ASC") }
-	scope :por_estado, joins(:estado).order("estados.nome, cidades.cidade")
+	scope :por_estado, lambda { joins(:estado).order("estados.nome, cidades.cidade") }
 	scope :publicados, lambda { where(:published => true) }
 	scope :despublicados, lambda { where(:published => false) }
 
