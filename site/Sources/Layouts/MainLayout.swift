aa9e21ffe205a6e75d77dd26662bef72ad9a8d51
Mauricio Cardozo <mauricio.cardozo@me.com>
2025-05-19 10:25:36 -0300

    adds appclip required meta tag

--- site/Sources/Layouts/MainLayout.swift
+++ site/Sources/Layouts/MainLayout.swift
@@ -3,6 +3,9 @@ import Ignite
 
 struct MainLayout: Layout {
     var body: Document {
+        Head {
+            MetaTag(name: "apple-itunes-app", content: "app-id=1180455342, app-clip-bundle-id=com.cocoaheads.conf.baseClip")
+        }
         Body {
             content
         }
